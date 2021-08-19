6e9dc31993d4bb21201330bf102d8b8b76ca2fbb
Meron Abate <meriab1234@gmail.com>
2021-08-19 11:57:20 +0300

    Inspection

--- client/src/views/Noncustomer.vue
+++ client/src/views/Noncustomer.vue
@@ -1,16 +1,8 @@
 <template>
-<v-container >
- <v-form
-    v-model="valid"
-    ref="form"
-    lazy-validation
-  >
- <div>
-    <v-toolbar
-    color="grey darken-4"
-    >
-    
-
+  <v-container>
+    <v-form>
+      <div>
+        <v-toolbar color="grey darken-4">
           <v-spacer></v-spacer>
           <v-btn icon color="white" route to="/">
             <v-icon>mdi-home</v-icon>
@@ -19,57 +11,45 @@
       </div>
 
       <v-spacer></v-spacer>
-<router-link
-        to="/"
-        tag="v-btn"
-      >
-      <v-btn icon color="white">
+
+      <v-btn icon color="white" route to="/">
         <v-icon>mdi-home</v-icon>
       </v-btn>
-</router-link>
-   
-    <v-text-field
-      v-model="Fname"
-      :counter="10"
-      label=" First Name"
-      required
-    ></v-text-field>
-     <v-text-field
-      v-model="Lname"
-      :counter="10"
-      label=" Last Name"
-      required
-    ></v-text-field>
-     <v-text-field
-      v-model="phone_no"
-      label="Phone number"
-      required
-    ></v-text-field>
-     <v-text-field
-      v-model="location"
-      :counter="10"
-      label="Location"
-      required
-    ></v-text-field>
-    <v-select
-      v-model="select"
-      :items="items"
-      :rules="[v => !!v || ' Case is required']"
-      label="Case"
-      required
-    ></v-select>
-    
-   
 
-    <v-btn
-      color="success"
-      class="mr-4"
-      @click="send"
-      :disabled="!valid"
-    >
-      Send
-      
-    </v-btn>
+      <v-text-field
+        v-model="Fname"
+        :counter="10"
+        label=" First Name"
+        required
+      ></v-text-field>
+      <v-text-field
+        v-model="Lname"
+        :counter="10"
+        label=" Last Name"
+        required
+      ></v-text-field>
+      <v-text-field
+        v-model="phone_no"
+        label="Phone number"
+        required
+      ></v-text-field>
+      <v-text-field
+        v-model="location"
+        :counter="10"
+        label="Location"
+        required
+      ></v-text-field>
+      <v-select
+        v-model="select"
+        :items="items"
+        :rules="[(v) => !!v || ' Case is required']"
+        label="Case"
+        required
+      ></v-select>
+
+      <v-btn color="success" class="mr-4" @click="send" :disabled="!valid">
+        Send
+      </v-btn>
 
       <v-btn color="success" class="mr-4" @click="validate">
         Send
@@ -81,55 +61,45 @@
 <script>
 import axios from "axios";
 export default {
-    name:"Noncustomer",
-    data: () => ({
-       valid: true,
-      Fname: '',
-      Lname: '',
-      phone_no: '',
-      location: '',
-      select: null,
-      items: [
-        'Transformer Burning',
-        'Fire',
-        'Fall down',
-        'Accident',
-        
-      ],
-    
+  name: "Noncustomer",
+  data: () => ({
+    valid: true,
+    Fname: "",
+    Lname: "",
+    phone_no: "",
+    location: "",
+    select: null,
+    items: ["Transformer Burning", "Fire", "Fall down", "Accident"],
+    validate: "",
     select: null,
     items: ["Fire", "Fall down", "Accident", "Rain"],
   }),
 
-   methods: {
-       send(){
-        let newEmergency ={
-          Fname: this.Fname,
-          Lname: this.Lname,
-          phone_no: this.phone_no,
-          location: this.location,
-          select: this.select,
-          
-        }; 
+  methods: {
+    send() {
+      let newEmergency = {
+        Fname: this.Fname,
+        Lname: this.Lname,
+        phone_no: this.phone_no,
+        location: this.location,
+        select: this.select,
+      };
       axios
         .post("http://localhost:3000/emergencys", newEmergency)
         .then(() => {
           this.$router.push({ path: "/" });
           this.$refs.form.reset();
 
-
-          this.get('/emergencys', (req,res)=>{
-            res.render('/')
-          })
-          
+          this.get("/emergencys", (req, res) => {
+            res.render("/");
+          });
         })
         .catch((err) => {
           console.log(err);
         });
-      
+
       return true;
     },
-    },
-}
-    
+  },
+};
 </script>
